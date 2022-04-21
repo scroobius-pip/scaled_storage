3639e27e1008328053016428e93a85ff3f6f631d
simdi <sim04ful@gmail.com>
2022-04-21 15:34:17 +0100

    Initial commit.

---
+++ src/scaled_storage_assets/src/index.js
@@ -0,0 +1,19 @@
+import { scaled_storage } from "../../declarations/scaled_storage";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await scaled_storage.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
